510e39d89db44ca7c1529349b6cc1267644b2ff7
Dan Digby <dandigby@me.com>
2021-05-11 15:32:50 +0100

    added post, put and delete requests

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const http = new easyHTTP;
 
-// get post
-http.get('https://jsonplaceholder.typicode.com/posts', function(error, response){
+// get posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(error, response){
+//   console.log(response);
+// });
+
+//get single post
+// http.get('https://jsonplaceholder.typicode.com/posts/5', function(error, response){
+//   console.log(response);
+// });
+
+//create data
+
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+//creat post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(error, response){
+//   console.log(response);
+// });
+
+//update post
+
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(error, response){
+//   console.log(response);
+// })
+
+//delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(error, response){
   console.log(response);
 });
